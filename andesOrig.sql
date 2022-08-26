d6fce66a2f97a37ebd93e9165d2e128b45757330
Ben Pesoa <benpesoa@gmail.com>
2022-08-26 04:23:09 -0300

    Nueva DB

--- andesOrig.sql
+++ andesOrig.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-08-2022 a las 07:20:51
+-- Tiempo de generación: 20-08-2022 a las 11:28:59
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
@@ -34,17 +34,17 @@ CREATE TABLE IF NOT EXISTS `novedades` (
   `cuerpo` text NOT NULL,
   `id_img` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `novedades`
 --
 
 INSERT INTO `novedades` (`id`, `titulo`, `cuerpo`, `id_img`) VALUES
-(23, '4 de Agosto', 'Al anochecer, el planeta Mercurio se ubicará 1° al este de la estrella Regulus, de la constelación de Leo. La curiosa conjunción se verá a baja altura (menos de 10º) sobre el horizonte del oeste. Podrá observarse a simple vista, aunque recomendamos el uso de binoculares.  (19:00 hs)', 'fdzkd28tkgsnws9oqe9l'),
-(24, '11 de Agosto', 'Saturno a 4º al noreste de la Luna. La conjunción podrá verse a simple vista y con binoculares. Con la Luna como “guía”, será una excelente oportunidad para identificar en el cielo al sexto planeta del Sistema Solar (21:00 hs). ', 'gvm34mv4praorhguzhuy'),
-(25, '14 de Agosto', 'Oposición de Saturno: el planeta se ubica en dirección exactamente opuesta al Sol en el cielo (de ahí el término). En torno a la oposición, la distancia entre la Tierra y un planeta externo es la menor en todo el año. En la noche de hoy, puntualmente, la distancia Tierra-Saturno será de “solo” 1.325 millones de kilómetros (en otros momentos puede ser de más de 1.600 millones de km.).', 'nqulavgdwl1ythxgb8i8'),
-(26, '15 de Agosto', 'Durante la madrugada, el brillantísimo Júpiter (mag -2.7) se verá a 3º al este-noreste de la Luna. Una impactante conjunción para disfrutar a simple vista y con binoculares (1:00 hs). ', 'ulldaezwnypp9jettpbs');
+(1, '15 de julio', 'Esta noche podremos ver a Saturno 4º al noroeste de la Luna. Una conjunción ideal para ver a simple vista y con binoculares. (23:00 hs)', 'c8dw1cvddhw3iaeg5ggo'),
+(2, '19 de julio', 'Durante la madrugada, Júpiter estará a solo 2,5º al oeste de la Luna. Otra notable conjunción para observar a ojo desnudo y con binoculares. (3:00 hs)', 'lb3txuetznitkulkfknm'),
+(3, '21 de julio', 'Una nueva conjunción en la alta madrugada: Marte se ubica a 3º al este la Luna. La escena ocurre a mediana altura sobre el cielo oriental y, por supuesto, podrá verse a simple vista.  (5:00 hs)', 'wwsnuoyp190frdo254uo'),
+(4, '26 de julio', 'Poco antes del amanecer, la Luna y Venus (separados por 4º) protagonizan una bonita conjunción a poca altura sobre el horizonte noroeste. Dada la claridad del crepúsculo matutino, se recomienda el uso de binoculares para una mejor observación. (7:15 hs)', 'njeyv7snl5zkypfs2lfj');
 
 -- --------------------------------------------------------
 
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -67,8 +67,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
 (1, 'ben', '81dc9bdb52d04dc20036dbd8313ed055'),
 (2, 'juan', '81dc9bdb52d04dc20036dbd8313ed055'),
-(3, 'pepe', '6074c6aa3488f3c2dddff2a7ca821aab'),
-(4, 'flavia', '81dc9bdb52d04dc20036dbd8313ed055');
+(3, 'pepe', '6074c6aa3488f3c2dddff2a7ca821aab');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
